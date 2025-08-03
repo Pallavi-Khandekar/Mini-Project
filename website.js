51a95d788bc14ed4457efbd3a400f237fe0eddc9
Pallavi_khandekar <khandekr234@gmail.com>
2025-08-03 07:38:44 +0530

    Initial Project Upload

---
+++ website.js
@@ -0,0 +1,30 @@
+<div id="contact">
+<div class="container">
+    <div class="row"> 
+        <div class="contact-left">
+           <h1 class="sub-title"> Contact me</h1>
+           <p><i class="fa-solid fa-paper-plane"></i>khandekr234@gmail.com</p>
+           <p><i class="fas fa-phone-square-alt"></i>9579436682</p>
+        <div class="social-icons">
+            <a href="https://facebook.com"><i class="fa-brands fa-facebook"></i></a>
+            <a href="https://twitter.com"><i class="fa-brands fa-twitter"></i></a>
+            <a href="https://instagram.com"><i class="fa-brands fa-instagram"></i></a>
+            <a href="https://linkedin.com"><i class="fa-brands fa-linkedin"></i></a>
+        </div>
+        <a href="images/my-cv.pdf" class="custom-button">download cv</a>
+    </div>
+        <div class="contact-right">
+            <form>
+                <input type="text" name="Name" placeholder="Your Name" required>
+                <input type="email" name="email" placeholder="Your Email" required>
+                <textarea name="Message" rows="4" placeholder="Your Message"></textarea>
+                <button type="submit" class="custom-button">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
+</div>
+<div class="copyright">
+    <p>copyright @ Pallavi Made with <i class="fa-solid fa-heart"></i> by Easy Tutorials</p>
+</div>
+</div>
